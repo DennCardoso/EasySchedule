de971e8ea5317efcbda122a715aa9c157d8c5db4
tsinja <thainespedro@gmail.com>
2015-04-21 23:07:28 -0500

    EasySchedule text

--- index.php
+++ index.php
@@ -15,11 +15,11 @@
     <h1>Welcome</h1>
   </div>
   <div id="main">
-    <p><b>Keep it simple</b> is a free template released by Reality Software under the <a target="_blank" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0</a> license, which means you can use it in any way you want provided you keep the link to the author intact.</p>
+    <p><b>EasySchedule</b> is a free software developed to help students to organize their class schedule and find the best hours to study. <!--<a target="_blank" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0</a> license, which means you can use it in any way you want provided you keep the link to the author intact.</p>
     <h2>Lorem ipsum dolor?</h2>
     <p>Praesent aliquam,  justo convallis luctus rutrum, erat nulla fermentum diam, at nonummy quam  ante ac quam. Quisque ornare risus quis  ligula. Phasellus tristique purus a augue condimentum adipiscing. Aenean  sagittis. Etiam leo pede, <a href="http://all-free-download.com/free-website-templates/">rhoncus venenatis</a>, tristique in, vulputate at,  odio. Donec et ipsum et sapien vehicula nonummy. Suspendisse potenti. Fusce  varius urna id quam.</p>
     <h2>Suspendisse potenti?</h2>
-    <p>Quisque ornare risus quis  ligula. Phasellus tristique purus a augue condimentum adipiscing. Aenean  sagittis. Etiam leo pede, rhoncus venenatis, tristique in, vulputate at,  odio. Donec et ipsum et sapien vehicula nonummy. Suspendisse potenti. Fusce  varius urna id quam. In eget purus. Integer <a href="http://all-free-download.com/free-website-templates/">turpis arcu</a>, pellentesque eget, cursus et, fermentum ut,  sapien. Fusce metus mi, eleifend sollicitudin, molestie id, varius et, nibh.</p>
+    <p>Quisque ornare risus quis  ligula. Phasellus tristique purus a augue condimentum adipiscing. Aenean  sagittis. Etiam leo pede, rhoncus venenatis, tristique in, vulputate at,  odio. Donec et ipsum et sapien vehicula nonummy. Suspendisse potenti. Fusce  varius urna id quam. In eget purus. Integer <a href="http://all-free-download.com/free-website-templates/">turpis arcu</a>, pellentesque eget, cursus et, fermentum ut,  sapien. Fusce metus mi, eleifend sollicitudin, molestie id, varius et, nibh.</p>-->
   </div>
   <div id="footer"> &copy;2007 Keep it Simple &nbsp;<span class="separator">|</span>&nbsp; Design by <a href="http://www.realitysoftware.ca">Reality Software</a> </div>
 </div>
